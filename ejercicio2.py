1519f228b1180e437ae763d06e7851d6964430f7
Unahur-Programación Concurrente-1-2023 <82103249+Unahur-Programacion-Concurrente@users.noreply.github.com>
2023-04-25 01:00:30 -0300

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,13 +1,15 @@
 import threading
 import time
 
+NUM = 5
+
 def algunHilo():
     print("\nHilo actual: {}".format(threading.current_thread()))
     print("\nHilo principal: {}".format(threading.main_thread()))
 
 hilos = []
 
-for i in range(3):
+for i in range(NUM):
     hilo = threading.Thread(target=algunHilo)
     hilo.start()
     hilos.append(hilo)
